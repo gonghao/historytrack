72c6b4a81b7fd2cfcb651b366de199e9d1005148
gonghao <gonghao@ghsky.com>
2012-05-13 16:48:22 +0800

    bug fix

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, g, render_template, session, request, make_response, url_for, redirect
 from jinja2 import Markup
-import uuid, time, urllib, urllib2, re
+import uuid, time, urllib, urllib2, re, sys
 
 # configuration
 DATABASE = 'historytrack.db'
@@ -54,12 +54,15 @@ def teardown_request(exception):
 def index():
     return render_template('index.html')
 
-@app.route('/link/<path:path>/')
+REG_CHARSET_SIMPLE = re.compile(r'<meta\s*charset\s*=\s*([\"\'])([^\"\']+)\1\s*>')
+REG_CHARSET = re.compile(r'<meta[\s\w=\"\'/;-]+charset=([^\'\"]+)')
+
+@app.route('/link/<path:path>')
 def link(path):
     if path.find(request.url_root) >= 0:
         return redirect(request.referrer)
 
-    referrer = url_for('index')
+    referrer = request.url_root
     if request.referrer:
         reg = re.compile(request.url_root + 'link/(.+)$')
         referrer = urllib.unquote_plus(request.referrer)
@@ -68,12 +71,16 @@ def link(path):
             referrer = urllib.unquote_plus(m.group(1))
 
     headers = request.headers
+    if request.method == 'GET' and request.query_string:
+        path = '%s?%s' % (path, request.query_string)
+
+    app.logger.debug('try to get content from: %s' % path)
+
     req = urllib2.Request(path, headers=dict([ ('User-Agent', headers.get('User-Agent')) ]))
 
     try:
         app.logger.debug('fetching content...')
         site = urllib2.urlopen(req)
-        app.logger.debug('fetching done')
     except urllib2.HTTPError:
         return redirect(request.referrer)
 
@@ -81,30 +88,63 @@ def link(path):
 
     app.logger.debug('try to get content...')
     content = site.read()
-    app.logger.debug('get content done')
+
+    charset = None
     if content_type.find('text/html') >= 0:
         app.logger.debug('try to decode source file...')
         add_track_record(referrer, path)
-        m = re.search('charset=([\w-]+)$', content_type)
+
+        decoded = None
+
+        # use content charset to dectect like: <meta charset="utf-8">
+        m = REG_CHARSET_SIMPLE.search(content)
         if m:
-            charset = m.group(1)
-            decoded = content.decode(charset)
+            charset = m.group(2)
+            try:
+                decoded = content.decode(charset)
+            except UnicodeDecodeError:
+                decoded = None
+
+        # use content charset to dectect like: <meta http-equiv="Content-Type" ...>
         else:
-            charsets = [ 'gbk', 'utf-8' ]
+            m = REG_CHARSET.search(content)
+            if m:
+                charset = m.group(1)
+                try:
+                    decoded = content.decode(charset)
+                except UnicodeDecodeError:
+                    decoded = None
+
+        # use header info to detect
+        if decoded is None:
+            m = REG_CHARSET.search(content_type)
+            if m:
+                charset = m.group(1)
+                try:
+                    decoded = content.decode(charset)
+                except UnicodeDecodeError:
+                    decoded = None
+
+        # fallback
+        if decoded is None:
+            charsets = [ 'gbk', 'gb2312', 'utf-8' ]
             charsets.reverse()
             while len(charsets) > 0:
                 try:
-                    decoded = content.decode(charsets.pop())
+                    charset = charsets.pop()
+                    decoded = content.decode(charset)
                     break
                 except UnicodeDecodeError:
                     continue
 
-        app.logger.debug('encode to utf-8')
-        content = decoded.encode('utf-8')
-        app.logger.debug('insert script tag')
+        app.logger.debug('decoded with %s' % charset)
+        #app.logger.debug('encode to utf-8')
+        #content = decoded.encode('utf-8')
+        #app.logger.debug('insert script tag')
         content = content.replace('</body>', '<script src="%s"></script></body>' % url_for('static', filename='js/core.js'))
 
-    return make_response(content)
+
+    return make_response((content, 200, { 'Content-Type': 'text/html; charset=%s' % charset })) if charset is not None else make_response(content)
 
 @app.route('/record')
 def list_records():
--- static/js/core.js
+++ static/js/core.js
@@ -1,18 +1,47 @@
 (function() {
     var doc = document;
 
-    function init() {
-        var as = doc.getElementsByTagName('a'),
-            a,
-            i = 0, len = as.length,
-            hostName = location.host;
+    var fakeLink = doc.createElement('a'),
+        hostName = location.host;
 
-        for (; i < len; i++) {
+    function adjustUrl(url) {
+        var pathname;
+
+        fakeLink.href = url;
+
+        if (fakeLink.protocol !== 'http:') {
+            return url;
+        }
+
+        if (hostName === fakeLink.host) {
+            if ((pathname = fakeLink.pathname) && pathname.indexOf('/link/') !== 0) {
+                fakeLink.href = location.pathname.replace('/link/', '');
+                url = location.origin + '/link/' + fakeLink.origin + pathname;
+            }
+        } else {
+            url = location.origin + '/link/' + url;
+        }
+
+        return url;
+    }
+
+    function init() {
+        var i, len;
+
+        var as = doc.getElementsByTagName('a'), a;
+
+        for (i = 0, len = as.length; i < len; i++) {
             a = as[i];
 
-            if (hostName !== a.host && a.protocol === 'http:') {
-                a.href = '/link/' + a.href;
-            }
+            a.href = adjustUrl(a.href);
+        }
+
+        var forms = doc.getElementsByTagName('form'), f;
+
+        for (i = 0, len = forms.length; i < len; i++) {
+            f = forms[i];
+
+            f.action = adjustUrl(f.action);
         }
     }
 
